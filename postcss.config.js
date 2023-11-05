b2907a8193ef94248198150662c41cc4eb447b3d
raselrafi <raselrafi536@gmail.com>
2023-11-05 13:41:24 +0600

    darco website project init

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        bounce: 'bounce 0.3s infinite'
+      },
+    },
+  },
+  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  
+  theme: {
+    extend: {},
+  },
+  variants: {
+    
+    extend:{
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
+
